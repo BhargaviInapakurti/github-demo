4827f8bcaacf28d93e8c711b4576e6e219240d4e
BhargaviInapakurti <100347358+BhargaviInapakurti@users.noreply.github.com>
2022-03-17 11:42:23 +0530

    Initial commit of math.py

---
+++ math.py
@@ -0,0 +1,17 @@
+
+# Add implementation
+def add(x,y):
+    return (x+y)
+# Subtract implementatiion
+def subtract(x,y):
+    return x-y
+# multiply implementation
+def multiply(x,y):
+<<<<<<< HEAD
+    return x*y                 # on master branch
+=======
+    return x*y                  # on Bug456
+>>>>>>> Bug456
+# divide implementation
+def divide(x,y):
+    return x/y
